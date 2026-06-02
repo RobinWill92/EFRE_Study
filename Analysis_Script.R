7346e119a8c021b659ea4a88acb792c11439678e
Robin <you@example.comrobin.willardt@web.de>
2026-06-02 13:57:27 +0200

    finished going through data list

--- Analysis_Script.R
+++ Analysis_Script.R
@@ -59,6 +59,21 @@ table(data_basic$CN15, useNA = "ifany") # 109 that answered Manip_Check 2 wrong;
 table(data_basic$BF01_22, useNA = "ifany") # 2 that answered Manip_Check BFI-K wrong; 10 that did not answer at all; correct is 5
 table(data_basic$CS01_25, useNA = "ifany") # 7 that answered Manip_Check BFI-K wrong; 10 that did not answer at all; correct is 1
 
+##
+# Demographics
+##
+
+# Sex
+table(data_basic$DM02)
+
+# Gender
+table(data_basic$DM03)
+
+# Education
+table(data_basic$DM06)
+
+### note: there are more detailed demos such as current job etc.; can be added here
+
 ###########################
 # Computing variables
 ###########################
@@ -229,6 +244,302 @@ data_basic %>%
 
 
 
+##
+# Naughty Nine !!!!nur 8 items!!!! ??????
+##
+
+
+# to be reverse-coded: ???
+
+# occuring values
+data_basic %>%
+  select(DT01_01, DT01_02, DT01_03, DT01_04, DT01_05, DT01_06, DT01_07, DT01_08) %>%
+  unlist() %>%
+  unique() 
+
+#reverse code
+
+# correlation matrix to see whether reverse items are scored in correct way
+
+data_basic %>%
+  select(DT01_01, DT01_02, DT01_03, DT01_04, DT01_05, DT01_06, DT01_07, DT01_08) %>%
+  cor(use = "pairwise.complete.obs") %>% round(2)
+
+# Cronbach's alpha
+
+data_basic %>%
+  select(DT01_01, DT01_02, DT01_03, DT01_04, DT01_05, DT01_06, DT01_07, DT01_08) %>%  psych::alpha()
+
+
+
+##
+# Self-efficiacy
+##
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(GS01_07, GS01_09, GS01_18, GS01_24, GS01_25, GS01_27, GS01_30, GS01_31, GS01_35, GS01_38) %>%
+  unlist() %>%
+  unique() 
+
+#reverse code
+
+# correlation matrix to see whether reverse items are scored in correct way
+
+data_basic %>%
+  select(GS01_07, GS01_09, GS01_18, GS01_24, GS01_25, GS01_27, GS01_30, GS01_31, GS01_35, GS01_38) %>%
+  cor(use = "pairwise.complete.obs") %>% round(2)
+
+# Cronbach's alpha
+
+data_basic %>%
+  select(GS01_07, GS01_09, GS01_18, GS01_24, GS01_25, GS01_27, GS01_30, GS01_31, GS01_35, GS01_38) %>%  psych::alpha()
+
+
+##
+# Erfindungen ordnen
+##
+
+# 01 = Buchdruck
+# 02 = KI
+# 03 = Smartphone
+# 04 = Internet
+# 05 = Automobil
+# 06 = Computer
+# 07 = Eisenbahn
+
+
+# occuring values
+
+data_basic %>%
+  select(JF02_01, JF02_02, JF02_03, JF02_04, JF02_05, JF02_06, JF02_07) %>%
+  unlist() %>%
+  unique() 
+
+
+##
+# Nutzung KI außerhalb des Arbeitsplatzes
+##
+
+# occuring values
+
+data_basic %>%
+  select(JF03_01) %>%
+  unlist() %>%
+  unique() 
+
+
+##
+# Nutzung KI innerhalb des Arbeitsplatzes
+##
+
+# occuring values
+
+data_basic %>%
+  select(JF04_01) %>%
+  unlist() %>%
+  unique() 
+
+
+##
+# PTM-R Prosocial Tendencies
+##
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(PT01_04, PT01_10, PT01_16, PT01_20, PT01_23, PT01_24) %>%
+  unlist() %>%
+  unique() 
+
+#reverse code
+
+# correlation matrix to see whether reverse items are scored in correct way
+
+data_basic %>%
+  select(PT01_04, PT01_10, PT01_16, PT01_20, PT01_23, PT01_24) %>%
+  cor(use = "pairwise.complete.obs") %>% round(2)
+
+# Cronbach's alpha
+
+data_basic %>%
+  select(PT01_04, PT01_10, PT01_16, PT01_20, PT01_23, PT01_24) %>%  psych::alpha()
+
+
+
+##
+# Trait Anxiety
+##
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(SA01_01, SA01_02, SA01_03, SA01_04, SA01_05, SA01_06, SA01_07, SA01_08, SA01_09, SA01_10,
+         SA01_11, SA01_12, SA01_13, SA01_14, SA01_15, SA01_16, SA01_17, SA01_18, SA01_19, SA01_20) %>%
+  unlist() %>%
+  unique() 
+
+#reverse code
+
+# correlation matrix to see whether reverse items are scored in correct way
+
+data_basic %>%
+  select(SA01_01, SA01_02, SA01_03, SA01_04, SA01_05, SA01_06, SA01_07, SA01_08, SA01_09, SA01_10,
+         SA01_11, SA01_12, SA01_13, SA01_14, SA01_15, SA01_16, SA01_17, SA01_18, SA01_19, SA01_20) %>%
+  cor(use = "pairwise.complete.obs") %>% round(2)
+
+# Cronbach's alpha
+
+data_basic %>%
+  select(SA01_01, SA01_02, SA01_03, SA01_04, SA01_05, SA01_06, SA01_07, SA01_08, SA01_09, SA01_10,
+         SA01_11, SA01_12, SA01_13, SA01_14, SA01_15, SA01_16, SA01_17, SA01_18, SA01_19, SA01_20) %>%  psych::alpha()
+
+
+
+##
+# Trait Anger
+##
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(SX02_01, SX02_02, SX02_03, SX02_04, SX02_05,
+         SX02_06, SX02_07, SX02_08, SX02_09, SX02_10) %>%
+  unlist() %>%
+  unique() 
+
+
+#######
+# Scenario Depression
+#######
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(SC06_01, SC06_02, SC06_08, SC06_04,
+         SC12_01, SC12_02, SC12_08, SC12_04,
+         SC13_01, SC13_02, SC13_08, SC13_04,
+         SC14_01, SC14_02, SC14_08, SC14_04,
+         SC15_01, SC15_02, SC15_08, SC15_04) %>%
+  unlist() %>%
+  unique() 
+
+#
+# correlation + Cronbach for Scenario 1
+
+data_basic %>%
+  select(SC06_01, SC06_02, SC06_08, SC06_04) %>%
+  cor(use = "pairwise.complete.obs") %>% round(2)
+
+# Cronbach's alpha
+
+data_basic %>%
+  select(SC06_01, SC06_02, SC06_08, SC06_04) %>%  psych::alpha()
+
+
+
+
+#######
+# Scenario Psychopathy
+#######
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(SC07_09, SC07_10, SC07_11, SC07_12,
+         SC16_09, SC16_10, SC16_11, SC16_12,
+         SC17_09, SC17_10, SC17_11, SC17_12,
+         SC18_09, SC18_10, SC18_11, SC18_12,
+         SC19_09, SC19_10, SC19_11, SC19_12) %>%
+  unlist() %>%
+  unique() 
+
+#
+# correlation + Cronbach for Scenario 1
+
+data_basic %>%
+  select(SC07_09, SC07_10, SC07_11, SC07_12) %>%
+  cor(use = "pairwise.complete.obs") %>% round(2)
+
+# Cronbach's alpha
+
+data_basic %>%
+  select(SC07_09, SC07_10, SC07_11, SC07_12) %>%  psych::alpha()
+
+
+
+#######
+# Scenario Trust
+#######
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(SC08_13, SC08_14, SC08_15, SC08_16,
+         SC20_13, SC20_14, SC20_15, SC20_16,
+         SC21_13, SC21_14, SC21_15, SC21_16,
+         SC22_13, SC22_14, SC22_15, SC22_16,
+         SC23_13, SC23_14, SC23_15, SC23_16) %>%
+  unlist() %>%
+  unique() 
+
+#
+# correlation + Cronbach for Scenario 1
+
+data_basic %>%
+  select(SC08_13, SC08_14, SC08_15, SC08_16) %>%
+  cor(use = "pairwise.complete.obs") %>% round(2)
+
+# Cronbach's alpha
+
+data_basic %>%
+  select(SC08_13, SC08_14, SC08_15, SC08_16) %>%  psych::alpha()
+
+
+
+#######
+# Leadership Szenario Items
+#######
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(SL04_01, SL04_02, SL04_09,      # prosocial items
+         SL04_08, SL04_04, SL04_10) %>%  # antisocial items
+  unlist() %>%
+  unique() 
+
+
+#######
+# Employees Szenario Items
+#######
+
+# to be reverse-coded: ???
+
+# occuring values
+
+data_basic %>%
+  select(SL05_02, SL05_03, SL05_04,     # prosocial items
+         SL05_01, SL05_05, SL05_06) %>%  # antisocial items
+  unlist() %>%
+  unique() 
 
 
 
@@ -239,6 +550,31 @@ data_basic %>%
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+##
+#
+##
+
+# to be reverse-coded: ???
+
+# occuring values
+
+#reverse code
+
+# correlation matrix to see whether reverse items are scored in correct way
+
+# Cronbach's alpha
 
 
 
