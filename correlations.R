baee59791eda5353b8374371575f7fe5192a99db
Jule Schilling <juleschilling@Jules-MacBook-Air.local>
2026-07-22 13:19:30 +0200

    summarize trust with all automation subscales (no trust in humans included)

--- correlations.R
+++ correlations.R
@@ -79,7 +79,13 @@ data_basic <- data_basic %>%
                                        AT01_07, AT01_08, AT01_09, AT01_10, AT01_11,
                                        AT01_12, AT01_13, AT01_14, AT01_15,
                                        AT01_16, AT01_17, AT01_18)), na.rm = TRUE))
-hist(data_basic$trust_all)
+#trust automation
+data_basic <- data_basic %>%
+  mutate(trust_automation_all = rowMeans(across(c(AT01_01, AT01_02, AT01_03,
+                                       AT01_04, AT01_05, AT01_06,
+                                       AT01_12, AT01_13, AT01_14, AT01_15,
+                                       AT01_16, AT01_17, AT01_18)), na.rm = TRUE))
+hist(data_basic$trust_automation)
 
 
 #3. other variables
